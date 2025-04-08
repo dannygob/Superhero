6878525ada38905403f120003af76b471c2015c0
tardes <dannygomezb@gmail.com>
2025-04-08 17:33:20 +0200

    add dependencias picasso y retrofit

--- app/src/main/java/com/example/superhero/data/SuperheroService.kt
+++ app/src/main/java/com/example/superhero/data/SuperheroService.kt
@@ -1,4 +1,12 @@
 package com.example.superhero.data
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface SuperheroService {
+    @GET("search/{name}")
+    suspend fun findSuperheroesByName(@Path("name") query: String): SuperheroResponse
+
+    @GET("{superhero-id}")
+    suspend fun findSuperheroById(@Path("superhero-id") id: String): Superhero
 }
