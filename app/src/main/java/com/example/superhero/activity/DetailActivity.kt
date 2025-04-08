3031a3b9c508c56c9a709669785ededa04a9eb5f
tardes <dannygomezb@gmail.com>
2025-04-08 17:18:18 +0200

    create the package data, activity and adapters

--- app/src/main/java/com/example/superhero/activity/DetailActivity.kt
+++ app/src/main/java/com/example/superhero/activity/DetailActivity.kt
@@ -1,43 +1,28 @@
 package com.example.superhero.activity
 
-//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
-
 import android.os.Bundle
-import android.view.View
-//import android.widget.ImageView
-//import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.superhero.R
-import com.example.superhero.data.Superhero
-import com.example.superhero.utils.SuperheroService
-import com.example.superhero.databinding.ActivityDetailBinding
-import com.squareup.picasso.Picasso
+import com.example.leagueofheroes.R
+import com.example.leagueofheroes.data.Superhero
+import com.example.leagueofheroes.data.SuperheroService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailActivity : AppCompatActivity() {
 
-    companion object {
-        const val SUPERHERO_ID = "SUPERHERO_ID"
-    }
-//    lateinit var nameTextView: TextView
-//    lateinit var avatarImageView: ImageView
-    lateinit var  binding : ActivityDetailBinding
     lateinit var superhero: Superhero
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -45,49 +30,29 @@ class DetailActivity : AppCompatActivity() {
         }
 
         val id = intent.getStringExtra("SUPERHERO_ID")!!
-
-//        nameTextView = findViewById(R.id.nameTextView)
-//        avatarImageView = findViewById(R.id.avatarImageView)
-
         getSuperheroById(id)
-
-        binding.navigationView.setOnItemSelectedListener { menuItem ->
-            binding.contentBiography.visibility = View.GONE
-            binding.contentAppearance.visibility = View.GONE
-            binding.contentStats.visibility = View.GONE
-
-            when (menuItem.itemId) {
-                R.id.menu_biography -> binding.contentBiography.visibility = View.VISIBLE
-                R.id.menu_appearance -> binding.contentAppearance.visibility = View.VISIBLE
-                R.id.menu_stats -> binding.contentStats.visibility = View.VISIBLE
-            }
-            true
-        }
-
-        binding.navigationView.selectedItemId = R.id.menu_biography
     }
 
-    fun getSuperheroById(id: String){
-    //fun getRetrofit(): SuperheroService {
+    fun loadData() {
+        Toast.makeText(this, superhero.name, Toast.LENGTH_LONG).show()
+        // Tenemos que rellenar la información del superheroe en pantalla
+    }
 
+    fun getRetrofit(): SuperheroService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.superheroapi.com/api.php/7252591128153666/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl("https://www.superheroapi.com/api.php/7252591128153666/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//        return retrofit.create(SuperheroService::class.java)
-//    }
-//
-//    fun getSuperheroById(id: String) {
+        return retrofit.create(SuperheroService::class.java)
+    }
 
-        //llamada en  el hilo secundario
+    fun getSuperheroById(id: String) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                val service = SuperheroService.getInstance()
+                val service = getRetrofit()
                 superhero = service.findSuperheroById(id)
 
-                //volvemos al hilo principal
                 CoroutineScope(Dispatchers.Main).launch {
                     loadData()
                 }
@@ -96,28 +61,4 @@ class DetailActivity : AppCompatActivity() {
             }
         }
     }
-
-    private fun loadData() {
-
-//        nameTextView.text = superhero.name
-        supportActionBar?.title = superhero.name
-        supportActionBar?.subtitle = superhero.biography.realName
-        Picasso.get().load(superhero.image.url).into(binding.avatarImageView)
-        Toast.makeText(this, superhero.name, Toast.LENGTH_LONG).show()
-
-
-        //biograhy
-        binding.publisherTextView.text = superhero.biography.publisher
-        binding.placeOfBirthTextView.text = superhero.biography.placeOfBirth
-        binding.alignmentTextView.text = superhero.biography.alignment
-
-        //Stat
-        binding.intelligenceTextView.text = "${superhero.stats.intelligence.toIntOrNull() ?: 0}"
-        binding.intelligenceProgress.progress = superhero.stats.intelligence.toIntOrNull() ?: 0
-
-    }
 }
-
-
-
-
--- app/src/main/java/com/example/superhero/activity/MainActivity.kt
+++ app/src/main/java/com/example/superhero/activity/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.superhero.activity
 
 import android.content.Intent
-import android.os.Binder
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
-import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.superhero.R
-import com.example.superhero.adapters.SuperheroAdapter
-import com.example.superhero.data.Superhero
-import com.example.superhero.utils.SuperheroService
-import com.example.superhero.databinding.ActivityMainBinding
+import com.example.leagueofheroes.R
+import com.example.leagueofheroes.adapters.SuperheroAdapter
+import com.example.leagueofheroes.data.Superhero
+import com.example.leagueofheroes.data.SuperheroService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -27,23 +23,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-
-    lateinit var binding: ActivityMainBinding
-    //lateinit var superhero: SuperheroAdapter
+    lateinit var recyclerView: RecyclerView
     lateinit var adapter: SuperheroAdapter
 
-    var superheroList: List<Superhero> = emptyList()
-
-   // lateinit var recyclerView: RecyclerView
-   //lateinit var adapter: SuperheroAdapter
-   // var superheroList: List<Superhero> = listOf()
+    var superheroList: List<Superhero> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -51,32 +40,32 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        //recyclerView = findViewById(R.id.recyclerView)
+        recyclerView = findViewById(R.id.recyclerView)
 
         adapter = SuperheroAdapter(superheroList) { position ->
             val superhero = superheroList[position]
 
             val intent = Intent(this, DetailActivity::class.java)
-            intent.putExtra(DetailActivity.SUPERHERO_ID, superhero.id)
+            intent.putExtra("SUPERHERO_ID", superhero.id)
             startActivity(intent)
         }
 
-        binding.recyclerView.adapter = adapter
-        binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
 
         searchSuperheroesByName("a")
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.activity_main_menu, menu)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_main, menu)
 
-        val menuItem = menu.findItem(R.id.menu_search)
-        val searchView = menuItem.actionView as SearchView
+        val menuItem = menu?.findItem(R.id.action_search)
+        val searchView = menuItem?.actionView as SearchView
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 searchSuperheroesByName(query)
-                return true
+                return false
             }
 
             override fun onQueryTextChange(query: String): Boolean {
@@ -87,30 +76,26 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-//    fun getRetrofit(): SuperheroService {
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl("https://www.superheroapi.com/api.php/7252591128153666/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//        return retrofit.create(SuperheroService::class.java)
-//    }
+    fun getRetrofit(): SuperheroService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.superheroapi.com/api.php/7252591128153666/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(SuperheroService::class.java)
+    }
 
     fun searchSuperheroesByName(query: String) {
-
-        //llamada a un hilo secundario
         CoroutineScope(Dispatchers.IO).launch {
             try {
-//                val service = getRetrofit()
-                val service = SuperheroService.getInstance()
-                val response = service.findSuperheroesByName(query)
-                superheroList = response.results
+                val service = getRetrofit()
+                val result = service.findSuperheroesByName(query)
 
+                superheroList = result.results
                 //volvemos al hilo principal
                 CoroutineScope(Dispatchers.Main).launch {
-//                    adapter.items = superheroList
-//                    adapter.notifyDataSetChanged()
-                    adapter.updateItems(superheroList)
+                    adapter.items = superheroList
+                    adapter.notifyDataSetChanged()
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
