8cdd59658412a436bbcc8a05530f481b5b4916d3
tardes <dannygomezb@gmail.com>
2025-04-08 17:24:29 +0200

    create the data SuperHero.kt

--- app/src/main/java/com/example/superhero/data/Superhero.kt
+++ app/src/main/java/com/example/superhero/data/Superhero.kt
@@ -1,38 +1,18 @@
 package com.example.superhero.data
 
-import com.google.android.material.color.utilities.Variant
-import com.google.gson.annotations.SerializedName
-
-
-data class SuperheroResponse (
-    //val response: String,
+class SuperheroResponse (
+    val response: String,
     val results: List<Superhero>
-)
+) {
 
-data class Superhero (
+}
+
+class Superhero (
     val id: String,
     val name: String,
-    val image: Image,
-    val biography: Biography,
-    @SerializedName("powerstats") val stats : Stats
-)
+    val image: Image
+) {
 
-data class  Biography(
-    @SerializedName("full-name") val realName : String,
-    @SerializedName("place-of-birth") val placeOfBirth : String,
-    val publisher: String,
-    val alignment: String
-    )
+}
 
-data class Stats (
-    var intelligence: String,
-    var strength: String,
-    var speed: String,
-    var durability: String,
-    var power: String,
-    var combat: String
-)
-
-
-data class Image (
-    val url: String)
+data class Image (val url: String)
