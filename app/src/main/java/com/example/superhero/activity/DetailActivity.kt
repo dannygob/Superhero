577cc1880f36bc7d25a94ca62719d4d8e7c5390b
tardes <dannygomezb@gmail.com>
2025-04-08 17:18:18 +0200

    create the package data, activity and adapters

--- app/src/main/java/com/example/superhero/activity/DetailActivity.kt
+++ app/src/main/java/com/example/superhero/activity/DetailActivity.kt
@@ -1,4 +1,4 @@
-package com.example.leagueofheroes.activities
+package com.example.superhero.activity
 
 import android.os.Bundle
 import android.widget.Toast
--- app/src/main/java/com/example/superhero/activity/MainActivity.kt
+++ app/src/main/java/com/example/superhero/activity/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.leagueofheroes.activities
+package com.example.superhero.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
