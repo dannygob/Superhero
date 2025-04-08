797c3cd69bb2b5f882686e1f7f82ae4c0f93795f
tardes <dannygomezb@gmail.com>
2025-04-08 18:55:49 +0200

    add libs and color and declared item superhero

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.superhero"
-        minSdk = 24
+        minSdk = 35
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
