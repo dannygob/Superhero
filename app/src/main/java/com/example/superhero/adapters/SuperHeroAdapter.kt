83de95b10dcc6259c16c879551ab7d3d4be8f2e3
tardes <dannygomezb@gmail.com>
2025-04-08 17:21:54 +0200

    create the class SuperHeroAdapter.kt

---
+++ app/src/main/java/com/example/superhero/adapters/SuperHeroAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.superhero.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.leagueofheroes.R
+import com.example.leagueofheroes.data.Superhero
+import com.example.superhero.R
+import com.squareup.picasso.Picasso
+
+class SuperheroAdapter(var items: List<Superhero>, val onClick: (Int) -> Unit) : Adapter<SuperheroViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+        return SuperheroViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: SuperheroViewHolder, position: Int) {
+        val superhero = items[position]
+        holder.render(superhero)
+        holder.itemView.setOnClickListener {
+            onClick(position)
+        }
+        fun updateItems (items: List<Superhero>){
+
+        }
+    }
+}
+
+class SuperheroViewHolder(view: View) : ViewHolder(view) {
+
+    var nameTextView: TextView = view.findViewById(R.id.nameTextView)
+    var avatarImageView: ImageView = view.findViewById(R.id.avatarImageView)
+
+    fun render(superhero: Superhero) {
+        nameTextView.text = superhero.name
+        Picasso.get().load(superhero.image.url).into(avatarImageView);
+    }
+}
