60cb361ee62b6af12b09354e37cb8530df5229c0
tardes <dannygomezb@gmail.com>
2025-04-08 17:21:54 +0200

    create the class SuperHeroAdapter.kt

--- app/src/main/java/com/example/superhero/adapters/SuperHeroAdapter.kt
+++ app/src/main/java/com/example/superhero/adapters/SuperHeroAdapter.kt
@@ -1,51 +1,45 @@
 package com.example.superhero.adapters
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-//import android.widget.ImageView
-//import android.widget.TextView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-//import com.example.superhero.R
-import com.example.superhero.data.Superhero
-import com.example.superhero.databinding.ItemSuperheroBinding
+import com.example.leagueofheroes.R
+import com.example.leagueofheroes.data.Superhero
+import com.example.superhero.R
 import com.squareup.picasso.Picasso
-import android.view.LayoutInflater
 
-class SuperheroAdapter(
-    var items: List<Superhero>,
-    val onItemClick: (position:Int) -> Unit
-): Adapter<SuperheroViewHolder>() {
+class SuperheroAdapter(var items: List<Superhero>, val onClick: (Int) -> Unit) : Adapter<SuperheroViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
-        //val view = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
-        val binding = ItemSuperheroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return SuperheroViewHolder(binding)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+        return SuperheroViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return  items.size
-    }
+    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: SuperheroViewHolder, position: Int) {
         val superhero = items[position]
         holder.render(superhero)
         holder.itemView.setOnClickListener {
-            onItemClick(position)
+            onClick(position)
+        }
+        fun updateItems (items: List<Superhero>){
+
         }
     }
-
-    fun updateItems(items: List<Superhero>){
-        this.items = items
-        notifyDataSetChanged()
-    }
-
 }
 
-class SuperheroViewHolder(val binding: ItemSuperheroBinding) : ViewHolder(binding.root) {
-    //data class Biography(
+class SuperheroViewHolder(view: View) : ViewHolder(view) {
+
+    var nameTextView: TextView = view.findViewById(R.id.nameTextView)
+    var avatarImageView: ImageView = view.findViewById(R.id.avatarImageView)
 
     fun render(superhero: Superhero) {
-        binding.nameTextView.text = superhero.name
-        Picasso.get().load(superhero.image.url).into(binding.avatarImageView)
+        nameTextView.text = superhero.name
+        Picasso.get().load(superhero.image.url).into(avatarImageView);
     }
 }
