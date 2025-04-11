063998e49d209b191969dc7d5a2d6ff6a7f96ec7
Superhola <68669782+dannygob@users.noreply.github.com>
2025-04-11 18:39:40 +0200

    reorder disorder

--- app/src/main/java/com/example/superhero/activity/DetailActivity.kt
+++ app/src/main/java/com/example/superhero/activity/DetailActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.superhero.R
 import com.example.superhero.data.Superhero
 import com.example.superhero.data.SuperheroService
+import com.example.superhero.databinding.ActivityDetailBinding
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,15 +24,18 @@ class DetailActivity : AppCompatActivity() {
     companion object{
         const val SUPERHERO_ID = "SUPERHERO_ID"
     }
-
-    lateinit var nameTextView: TextView
-    lateinit var avatarImageView: ImageView
+//    lateinit var nameTextView: TextView
+//    lateinit var avatarImageView: ImageView
+    lateinit var  binding : ActivityDetailBinding
     lateinit var superhero: Superhero
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -40,37 +44,40 @@ class DetailActivity : AppCompatActivity() {
 
         val id = intent.getStringExtra("SUPERHERO_ID")!!
 
-
-        nameTextView = findViewById(R.id.nameTextView)
-        avatarImageView = findViewById(R.id.avatarImageView)
+//        nameTextView = findViewById(R.id.nameTextView)
+//        avatarImageView = findViewById(R.id.avatarImageView)
 
         getSuperheroById(id)
 
-        binding.navigationView.setOnItemSelectedListener {
-            bindin.contenBiography.visibility = View.GONE
-            bindin.contenApareance.visibility = View.GONE
-            bindin.contenStats.visibility = View.GONE
+        binding.navigationView.setOnItemSelectedListener {menuItem ->
+            binding.contentBiography.visibility = View.GONE
+            binding.contentApareance.visibility = View.GONE
+            binding.contentStats.visibility = View.GONE
 
             when (menuItem.itemId){
                 R.id.menu_biography -> binding.contentBiography.visibility = View.VISIBLE
                 R.id.menu_appareance -> binding.contentAppareance.visibility = View.VISIBLE
                 R.id.menu_stats -> binding.contentStat.visibility = View.VISIBLE
             }
+            true
         }
 
-
+        binding.navigationView.selectedItemId = R.id.menu_biography
     }
 
-    fun getRetrofit(): SuperheroService {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://www.superheroapi.com/api.php/7252591128153666/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    fun getSuperheroById(id: String){
+    //fun getRetrofit(): SuperheroService {
 
-        return retrofit.create(SuperheroService::class.java)
-    }
 
-    fun getSuperheroById(id: String) {
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl("https://www.superheroapi.com/api.php/7252591128153666/")
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//
+//        return retrofit.create(SuperheroService::class.java)
+//    }
+//
+//    fun getSuperheroById(id: String) {
 
         //llamada en  el hilo secundario
         CoroutineScope(Dispatchers.IO).launch {
@@ -88,13 +95,24 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    fun LoadData() {
-        nameTextView.text = superhero.name
-      Picasso.get().load(superhero.image.url).into(avatarImageView)
+    private fun LoadData() {
+
+//        nameTextView.text = superhero.name
+        supportActionBar?.title = superhero.name
+        supportActionBar?.subtitle = superhero. biography.realName
+        Picasso.get().load(superhero.image.url).into(binding.avatarImageView)
         Toast.makeText(this, superhero.name, Toast.LENGTH_LONG).show()
 
+
         //biograhy
-        binding.intelligenceTexteView.text = "${superhero.stats.intellgence}
-        binding.intelligenceProgress.progress = superhero.stats.intelligence.toIn
+        binding.plublisherTextView.text = superhero.biography.publisher
+        binding.placedOfBirthTextView.text = superhero.biography.placeOfBirth
+        binding.alignmentTextView.TextView.text = superhero.biography.alignmente
+
+        //Stat
+        binding.intelligenceTexteView.text = "${superhero.stats.intellgence.toIntOrNull() ?:0}"
+        binding.intelligenceProgress.progress = superhero.stats.intelligence.toInOrNull() ?:0
+
+
     }
 }
