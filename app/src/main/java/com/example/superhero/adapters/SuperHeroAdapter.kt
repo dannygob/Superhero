3ff99bb1108dc9f22f28c393d8f8dd22f916a1fc
Superhola <68669782+dannygob@users.noreply.github.com>
2025-04-11 18:39:40 +0200

    disorder

--- app/src/main/java/com/example/superhero/adapters/SuperHeroAdapter.kt
+++ app/src/main/java/com/example/superhero/adapters/SuperHeroAdapter.kt
@@ -9,12 +9,14 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.superhero.R
 import com.example.superhero.data.Superhero
+import com.example.superhero.databinding.ItemSuperheroBinding
 import com.squareup.picasso.Picasso
 
 class SuperheroAdapter(var items: List<Superhero>, val onClick: (position:Int) -> Unit) : Adapter<SuperheroViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+        //val view = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+        val binding = ItemSuperheroBinding.inflate(layoutInflater.fr)
         return SuperheroViewHolder(view)
     }
 
@@ -32,7 +34,13 @@ class SuperheroAdapter(var items: List<Superhero>, val onClick: (position:Int) -
     }
 }
 
-class SuperheroViewHolder(view: View) : ViewHolder(view) {
+class SuperheroViewHolder(binding: ItemSuperheroBinding) : ViewHolder(binding.root) {
+    data class Biography(
+        val
+        val
+        val
+
+            )
 
     var nameTextView: TextView = view.findViewById(R.id.nameTextView)
     var avatarImageView: ImageView = view.findViewById(R.id.avatarImageView)
